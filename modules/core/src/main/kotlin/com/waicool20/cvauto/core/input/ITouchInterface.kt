6e9d4049e882e3c29d51b4e8925a2b724aef6db3
waicool20 <waicool20@gmail.com>
2019-11-11 00:30:19 +0800

    Core: Designed preliminary touch api

--- modules/core/src/main/kotlin/com/waicool20/cvauto/core/input/ITouchInterface.kt
+++ modules/core/src/main/kotlin/com/waicool20/cvauto/core/input/ITouchInterface.kt
@@ -1,5 +1,22 @@
 package com.waicool20.cvauto.core.input
 
 interface ITouchInterface {
-// TODO
+    /**
+     * Represents the state of a touch
+     *
+     * @param cursorX x coordinate of this touch
+     * @param cursorY y coordinate of this touch
+     * @param isTouching True if screen is touched at this coordinate
+     */
+    data class Touch(
+        val slot: Int,
+        var cursorX: Int = 0,
+        var cursorY: Int = 0,
+        var isTouching: Boolean = false
+    )
+
+    val touches: List<Touch>
+    fun touchUp(slot: Int)
+    fun touchDown(slot: Int)
+    fun touchMove(slot: Int, x: Int, y: Int)
 }
