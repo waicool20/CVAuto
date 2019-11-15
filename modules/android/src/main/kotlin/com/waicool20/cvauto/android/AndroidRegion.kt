ea1ffc01c8b672a7ee483af73b96d07f087ef1a0
waicool20 <waicool20@gmail.com>
2019-11-15 21:18:57 +0800

    Android: Add android specific input convenience functions

--- modules/android/src/main/kotlin/com/waicool20/cvauto/android/AndroidRegion.kt
+++ modules/android/src/main/kotlin/com/waicool20/cvauto/android/AndroidRegion.kt
@@ -1,7 +1,10 @@
 package com.waicool20.cvauto.android
 
+import com.waicool20.cvauto.core.Millis
 import com.waicool20.cvauto.core.Pixels
 import com.waicool20.cvauto.core.Region
+import com.waicool20.cvauto.core.input.IInput
+import com.waicool20.cvauto.core.input.ITouchInterface
 import com.waicool20.cvauto.util.matching.ITemplateMatcher
 import java.awt.Rectangle
 import java.awt.color.ColorSpace
@@ -92,6 +95,43 @@ class AndroidRegion(
     override fun type(text: String) {
         click()
         device.input.keyboard.type(text)
+    }
+
+    /**
+     * Swipes from this region to the other region
+     * For more complex operations please use the devices respective [IInput]
+     *
+     * @param other Other region to swipe to
+     * @param duration Duration of the swipe
+     * @param random If true the source and destination location will be random points in their respective regions
+     */
+    fun swipeTo(other: AndroidRegion, duration: Millis = 1000, random: Boolean = true) {
+        val swipe = if (random) {
+            val src = randomPoint()
+            val dest = other.randomPoint()
+            ITouchInterface.Swipe(0, src.x, src.y, dest.x, dest.y)
+        } else {
+            ITouchInterface.Swipe(0,
+                centerX.roundToInt(), centerY.roundToInt(),
+                other.centerX.roundToInt(), other.centerY.roundToInt()
+            )
+        }
+        device.input.touchInterface?.swipe(swipe, duration)
+    }
+
+    /**
+     * Sends a pinch gesture, centered at this regions center
+     *
+     * @param r1 Starting radius of the pinch
+     * @param r2 End radius of the pinch
+     * @param angle Pinch angle
+     * @param duration Duration of the pinch
+     */
+    fun pinch(r1: Int, r2: Int, angle: kotlin.Double, duration: Millis = 1000) {
+        device.input.touchInterface?.pinch(
+            centerX.roundToInt(), centerY.roundToInt(),
+            r1, r2, angle, duration
+        )
     }
 
     override fun clone(): Any {
