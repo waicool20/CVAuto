5d6cff80df044b95558f9c9a59c626bd40f24df8
waicool20 <waicool20@gmail.com>
2019-11-15 21:39:20 +0800

    Core: Implement image template

---
+++ modules/core/src/main/kotlin/com/waicool20/cvauto/core/template/ImageTemplate.kt
@@ -0,0 +1,23 @@
+package com.waicool20.cvauto.core.template
+
+import java.awt.image.BufferedImage
+import java.net.URI
+
+class ImageTemplate(
+    private val image: BufferedImage,
+    override val threshold: Double? = null
+) : ITemplate {
+    override val source: URI = URI.create("image://${hashCode()}")
+
+    override fun load(): BufferedImage {
+        return image
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return other is ImageTemplate && image == other.image
+    }
+
+    override fun hashCode(): Int {
+        return image.hashCode()
+    }
+}
