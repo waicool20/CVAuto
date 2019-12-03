096ccf8212c9dd5f24eec68b4675f4eef52fbc3c
waicool20 <waicool20@gmail.com>
2019-12-03 15:44:22 +0800

    Add Rectangle convenience functions

--- modules/core/src/main/kotlin/com/waicool20/cvauto/util/RectangleExtensions.kt
+++ modules/core/src/main/kotlin/com/waicool20/cvauto/util/RectangleExtensions.kt
@@ -6,3 +6,30 @@ import java.awt.Rectangle
  * Area of the rectangle
  */
 val Rectangle.area get() = width * height
+
+/**
+ * Convenience value, equivalent to [Rectangle.x]
+ */
+val Rectangle.x1 get() = x
+
+/**
+ * Convenience value, equivalent to [Rectangle.x] + [Rectangle.width]
+ */
+val Rectangle.x2 get() = x + width
+
+/**
+ * Convenience value, equivalent to [Rectangle.y]
+ */
+val Rectangle.y1 get() = y
+
+/**
+ * Convenience value, equivalent to [Rectangle.y] + [Rectangle.height]
+ */
+val Rectangle.y2 get() = y + height
+
+/**
+ * Crops and returns a new rect that fits in the other rect
+ */
+fun Rectangle.cropIntoRect(other: Rectangle): Rectangle {
+    return createIntersection(other).bounds
+}
