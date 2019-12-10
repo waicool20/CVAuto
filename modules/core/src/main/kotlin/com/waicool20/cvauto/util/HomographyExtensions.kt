e5c7c1f76baf935c5d90ba781088a9b57c5ffdaf
waicool20 <waicool20@gmail.com>
2019-12-10 11:39:15 +0800

    Core: Add Point conversion extensions; Add transformRect homography extensions

--- modules/core/src/main/kotlin/com/waicool20/cvauto/util/HomographyExtensions.kt
+++ modules/core/src/main/kotlin/com/waicool20/cvauto/util/HomographyExtensions.kt
@@ -1,9 +1,11 @@
+@file:Suppress("NOTHING_TO_INLINE")
 package com.waicool20.cvauto.util
 
 import georegression.struct.homography.Homography2D_F64
 import georegression.struct.point.Point2D_F64
 import georegression.struct.point.Point2D_I32
 import georegression.transform.homography.HomographyPointOps_F64
+import java.awt.Rectangle
 
 /**
  * Transform the given point based on this homography matrix
@@ -13,7 +15,6 @@ import georegression.transform.homography.HomographyPointOps_F64
  *
  * @return Transformed point
  */
-@Suppress("NOTHING_TO_INLINE")
 inline fun Homography2D_F64.transformPoint(x: Int, y: Int) = transformPoint(x.toDouble(), y.toDouble())
 
 /**
@@ -28,4 +29,29 @@ fun Homography2D_F64.transformPoint(x: Double, y: Double): Point2D_I32 {
     val result = Point2D_F64()
     HomographyPointOps_F64.transform(this, Point2D_F64(x, y), result)
     return Point2D_I32(result.x.toInt(), result.y.toInt())
+}
+
+/**
+ * Transform the given rectangle based on this homography matrix
+ *
+ * @param rect Rectangle to transform
+ *
+ * @return Transformed rectangle
+ */
+inline fun Homography2D_F64.transformRect(rect: Rectangle) = transformRect(rect.x, rect.y, rect.width, rect.height)
+
+/**
+ * Transform the given rectangle based on this homography matrix
+ *
+ * @param x x coordinate
+ * @param y y coordinate
+ * @param width rectangle width
+ * @param height rectangle height
+ *
+ * @return Transformed rectangle
+ */
+fun Homography2D_F64.transformRect(x: Int, y: Int, width: Int, height: Int): Rectangle {
+    val (x1, y1) = transformPoint(x, y)
+    val (x2, y2) = transformPoint(x + width, y + height)
+    return Rectangle(x1, y1, x2 - x1, y2 - y1)
 }
--- modules/core/src/main/kotlin/com/waicool20/cvauto/util/PointExtensions.kt
+++ modules/core/src/main/kotlin/com/waicool20/cvauto/util/PointExtensions.kt
@@ -1,6 +1,7 @@
 package com.waicool20.cvauto.util
 
 import georegression.struct.point.Point2D_I32
+import java.awt.Point
 
 /**
  * For destructuring point eg.
@@ -17,3 +18,13 @@ operator fun Point2D_I32.component1() = x
  * ```
  */
 operator fun Point2D_I32.component2() = y
+
+/**
+ * Converts from [Point2D_I32] to [Point]
+ */
+fun Point2D_I32.toAwtPoint() = Point(x, y)
+
+/**
+ * Converts from [Point] to [Point2D_I32]
+ */
+fun Point.toBoofPoint() = Point2D_I32(x, y)
