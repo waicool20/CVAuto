e699d21614bd83315771f2b7c9613fb744599241
waicool20 <waicool20@gmail.com>
2020-04-23 00:47:57 +0800

    Android: Make android input nullable to make consistent with non server version

--- modules/android/src/main/kotlin/com/waicool20/cvauto/android/input/server/AndroidServerInput.kt
+++ modules/android/src/main/kotlin/com/waicool20/cvauto/android/input/server/AndroidServerInput.kt
@@ -2,6 +2,7 @@ package com.waicool20.cvauto.android.input.server
 
 import com.waicool20.cvauto.android.AndroidDevice
 import com.waicool20.cvauto.core.input.IInput
+import com.waicool20.cvauto.core.input.ITouchInterface
 
 class AndroidServerInput internal constructor(device: AndroidDevice) : IInput {
     companion object {
@@ -11,5 +12,5 @@ class AndroidServerInput internal constructor(device: AndroidDevice) : IInput {
 
     override val mouse = null
     override val keyboard = AndroidServerKeyboard(device)
-    override val touchInterface = AndroidServerTouchInterface(device)
+    override val touchInterface: ITouchInterface? = AndroidServerTouchInterface(device)
 }
