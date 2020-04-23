311b8780900f5025421f523cb410a58d4d373d19
waicool20 <waicool20@gmail.com>
2020-04-23 10:00:26 +0800

    Update kotlin

--- modules/desktop/build.gradle.kts
+++ modules/desktop/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.3.72"
 }
 
 version = "1.0-SNAPSHOT"
@@ -10,7 +10,7 @@ version = "1.0-SNAPSHOT"
 dependencies {
     val versions = object {
         val Kotlin by lazy { plugins.getPlugin(KotlinPluginWrapper::class).kotlinPluginVersion }
-        val KotlinCoroutines = "1.3.2"
+        val KotlinCoroutines = "1.3.5"
     }
 
     implementation("org.jetbrains.kotlin", "kotlin-stdlib-jdk8", versions.Kotlin)
