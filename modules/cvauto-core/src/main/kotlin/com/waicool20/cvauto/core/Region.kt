e87f31e7624c0726db4364313948ca745cba9c68
waicool20 <waicool20@gmail.com>
2021-05-09 15:34:51 +0800

    Region: Increase default capture timeout

--- modules/cvauto-core/src/main/kotlin/com/waicool20/cvauto/core/Region.kt
+++ modules/cvauto-core/src/main/kotlin/com/waicool20/cvauto/core/Region.kt
@@ -49,7 +49,7 @@ abstract class Region<T : IDevice<T, R>, R : Region<T, R>>(
     companion object {
         val DEFAULT_MATCHER = DefaultTemplateMatcher()
         var FIND_REFRESH: Millis = 32
-        var CAPTURE_TIMEOUT: Millis = 5000
+        var CAPTURE_TIMEOUT: Millis = 10000
     }
 
     /**
