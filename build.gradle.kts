a589b9f289e5f1b7ab5811444b013005a745feda
waicool20 <waicool20@gmail.com>
2022-01-23 14:11:45 -0800

    Update gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         }
     }
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
     }
 }
 
