88b55167d0ccb51655204344add40cd037f0c554
waicool20 <waicool20@gmail.com>
2023-02-20 01:04:14 -0800

    Add github pages deploy

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.dokka") version "1.7.20"
 }
 
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.dokka")
 
     group = "com.waicool20.cvauto"
     version = "1.0-SNAPSHOT"
@@ -38,4 +40,12 @@ allprojects {
             compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
+}
+
+task<Zip>("docs") {
+    dependsOn("dokkaHtmlCollector")
+    destinationDirectory.set(file("$buildDir/dokka"))
+    from(file("$buildDir/dokka/htmlCollector"))
+    into("docs")
+    archiveFileName.set("docs.zip")
 }
