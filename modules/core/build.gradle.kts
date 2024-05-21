c259b62cfdd07207c511f4cd2c4eb51a14219b5c
waicool20 <waicool20@gmail.com>
2024-05-20 23:50:46 -0700

    Update libs

--- modules/core/build.gradle.kts
+++ modules/core/build.gradle.kts
@@ -4,15 +4,15 @@ dependencies {
         "macosx-x86_64",
         "windows-x86_64"
     )
-    val javacppVersion = "1.5.9"
+    val javacppVersion = "1.5.10"
     platforms.forEach { p ->
         api("org.bytedeco:javacpp:$javacppVersion")
         api("org.bytedeco:javacpp:$javacppVersion:$p")
-        api("org.bytedeco:opencv:4.7.0-$javacppVersion")
-        api("org.bytedeco:opencv:4.7.0-$javacppVersion:$p")
-        api("org.bytedeco:openblas:0.3.23-$javacppVersion")
-        api("org.bytedeco:openblas:0.3.23-$javacppVersion:$p")
-        api("org.bytedeco:ffmpeg:6.0-$javacppVersion")
-        api("org.bytedeco:ffmpeg:6.0-$javacppVersion:$p")
+        api("org.bytedeco:opencv:4.9.0-$javacppVersion")
+        api("org.bytedeco:opencv:4.9.0-$javacppVersion:$p")
+        api("org.bytedeco:openblas:0.3.26-$javacppVersion")
+        api("org.bytedeco:openblas:0.3.26-$javacppVersion:$p")
+        api("org.bytedeco:ffmpeg:6.1.1-$javacppVersion")
+        api("org.bytedeco:ffmpeg:6.1.1-$javacppVersion:$p")
     }
 }
